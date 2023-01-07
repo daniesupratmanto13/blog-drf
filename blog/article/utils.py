456169b846344bb37458cb06336258aa939229af
daniesupratmanto13 <daniesupratmanto@gmail.com>
2023-01-07 08:39:10 +0800

    modified function get_random_code and unique_slugify in article/utils.py

--- blog/article/utils.py
+++ blog/article/utils.py
@@ -2,11 +2,11 @@ from django.utils.text import slugify
 from uuid import uuid4
 
 
-def get_random_code():
-    return str(uuid4())[:8].replace('-', '').lower()
+def get_random_code(n: int) -> str:
+    return str(uuid4())[:n].upper()
 
 
-def unique_slugify(title, temp_title, slug, Model):
+def unique_slugify(title, temp_title, slug, Model) -> str:
 
     temp_slug = slug
 
@@ -15,7 +15,7 @@ def unique_slugify(title, temp_title, slug, Model):
             str(title))
         exists_slug = Model.objects.filter(slug=temp_slug).exists()
         while exists_slug:
-            temp_slug = slugify(temp_slug + ' ' + get_random_code())
+            temp_slug = slugify(temp_slug + ' ' + get_random_code(5))
             exists_slug = Model.objects.filter(
                 slug=temp_slug).exists()
 
