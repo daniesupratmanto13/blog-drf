41bf1d36372d31d7a8a6fb6fa8322e90af371872
daniesupratmanto13 <daniesupratmanto@gmail.com>
2023-01-30 09:15:14 +0800

    edit readme.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# django, drf
+## blog app
+## with auth token
+
+Clone:
+
+```sh
+git@github.com:daniesupratmanto13/blog-drf.git
+cd blog_django
+```
+
+Install it and run:
+
+```sh
+pipenv install
+pipenv shell
+cd blog
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+```
--- blog/article/urls.py
+++ blog/article/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.urlpatterns import format_suffix_patterns
+# from rest_framework.urlpatterns import format_suffix_patterns
 
 # views
 from . import views
@@ -18,12 +18,12 @@ urlpatterns = [
     path('comments/', views.CommentCreate.as_view(), name='comment-create'),
     path('likes/', views.LikeCreate.as_view(), name='like-create'),
     path('profiles/', views.ProfileList.as_view(), name='profile-list'),
-    # path('comments/', views.CommentList.as_view(), name='comment-list'),
     path('users/<str:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('articles/<str:pk>/', views.ArticleDetail.as_view(), name='article-detail'),
     path('likes/<str:pk>/', views.LikeDetail.as_view(), name='like-update'),
     path('profiles/<str:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
     path('', include(router.urls)),
+    # path('comments/', views.CommentList.as_view(), name='comment-list'),
     # path('comments/<str:pk>/', views.CommentDetail.as_view(), name='comment-detail'),
 ]
 
